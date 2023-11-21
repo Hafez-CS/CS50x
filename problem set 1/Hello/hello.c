b6e310f5a8a443bb783dd8ae5818f5708dd10885
Mohammad Hafez Najafi <151014739+Hafez-CS@users.noreply.github.com>
2023-11-21 10:30:47 +0330

    Update hello.c

--- problem set 1/Hello/hello.c
+++ problem set 1/Hello/hello.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
 
+int main(void)
+{
+    //you can say your name to machine and get the answer
+
+    string a = get_string("What's your name my friend? ");
+    // this line can get your name
+    printf("hello, %s\n", a);
+    // this line can say hello with your name
+}
